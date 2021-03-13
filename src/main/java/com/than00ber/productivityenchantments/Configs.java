9ca4753d7c585b8e8feac26ce5f12080d9bf1f02
greffnoah <greffnoah@gmail.com>
2021-03-13 14:24:35 +0100

    fixed config file

--- src/main/java/com/than00ber/productivityenchantments/Configs.java
+++ src/main/java/com/than00ber/productivityenchantments/Configs.java
@@ -1,6 +1,5 @@
 package com.than00ber.productivityenchantments;
 
-import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.common.ForgeConfigSpec;
 import org.apache.commons.lang3.tuple.Pair;
 
@@ -19,72 +18,40 @@ public class Configs {
     public static ForgeConfigSpec.EnumValue<CarveType> PLOWING_CARVE_TYPE;
     public static ForgeConfigSpec.EnumValue<CarveType> FERTILITY_CARVE_TYPE;
 
-    public Configs(ForgeConfigSpec.Builder forgeConfigBuilder) {
-        ConfigBuilder builder = new ConfigBuilder(forgeConfigBuilder);
+    public Configs(ForgeConfigSpec.Builder builder) {
 
-        PLANTING_SEEDS_DAMAGE_ITEM = builder.defineBoolean("planting_seeds_damage_item", true);
-        GROWING_CROPS_DAMAGE_ITEM = builder.defineBoolean("growing_seeds_damage_item", false);
-        PLACING_TORCH_DAMAGE_ITEM = builder.defineBoolean("placing_torch_damage_item", false);
+        PLANTING_SEEDS_DAMAGE_ITEM = builder
+                .comment("Determines whether hoe takes 1/2 of damage per seed planted with Fertility.")
+                .define("planting_seeds_damage_item", true);
+        GROWING_CROPS_DAMAGE_ITEM = builder
+                .comment("Determines whether hoe takes 1/2 of damage per bone meal when attempting to grow crops.")
+                .define("growing_crops_damage_item", false);
+        PLACING_TORCH_DAMAGE_ITEM = builder
+                .comment("Determines whether placing torches deals 1/2 of damage to the item.")
+                .define("placing_torch_damage_item", false);
 
-        WOODCUTTING_CARVE_TYPE = builder.defineEnum("woodcutting_carve_type", CarveType.CONNECTED, CarveType.values());
-        DIGGING_CARVE_TYPE = builder.defineEnum("digging_carve_type", CarveType.CONNECTED, CarveType.values());
-        CULTIVATION_CARVE_TYPE = builder.defineEnum("cultivation_carve_type", CarveType.ALL, CarveType.values());
-        PLOWING_CARVE_TYPE = builder.defineEnum("plowing_carve_type", CarveType.CONNECTED, CarveType.values());
-        FERTILITY_CARVE_TYPE = builder.defineEnum("fertility_carve_type", CarveType.CONNECTED, CarveType.values());
+        WOODCUTTING_CARVE_TYPE = builder
+                .comment("Change whether Woodcutting breaks all blocks within effective radius or only those connected.")
+                .comment("This does not apply to log-type blocks, it will continue to cut down entire trees.")
+                .defineEnum("woodcutting_carve_type", CarveType.CONNECTED, CarveType.values());
+        DIGGING_CARVE_TYPE = builder
+                .comment("Change whether Digging breaks all blocks within effective radius or only those connected.")
+                .defineEnum("digging_carve_type", CarveType.CONNECTED, CarveType.values());
+        CULTIVATION_CARVE_TYPE = builder
+                .comment("Change whether Cultivation breaks all crops within effective radius or only those connected.")
+                .defineEnum("digging_carve_type", CarveType.ALL, CarveType.values());
+        PLOWING_CARVE_TYPE = builder
+                .comment("Change whether Cultivation turns all dirt blocks into farmland within effective radius or only those connected.")
+                .defineEnum("plowing_carve_type", CarveType.CONNECTED, CarveType.values());
+        FERTILITY_CARVE_TYPE = builder
+                .comment("Change whether Fertility plants seeds within connected farmland or all within effective radius.")
+                .defineEnum("fertility_carve_type", CarveType.CONNECTED, CarveType.values());
     }
 
     static {
         Pair<Configs, ForgeConfigSpec> pair = new ForgeConfigSpec.Builder().configure(Configs::new);
         CONFIG_SPEC = pair.getRight();
         CONFIGS = pair.getLeft();
-    }
-
-    private static class ConfigBuilder {
-
-        private static ForgeConfigSpec.Builder BUILDER;
-
-        private ConfigBuilder(ForgeConfigSpec.Builder builder) {
-            BUILDER = builder;
-        }
-
-        public ForgeConfigSpec.BooleanValue defineBoolean(String key, boolean defaultValue) {
-            return BUILDER.comment(fromConfigKey(key), noteFromConfigKey(key)).define(key, defaultValue);
-        }
-
-        public ForgeConfigSpec.IntValue defineRange(String key, int defaultValue, int min, int max) {
-            return BUILDER.comment(fromConfigKey(key), noteFromConfigKey(key)).defineInRange(key, defaultValue, min, max);
-        }
-
-        public ForgeConfigSpec.DoubleValue defineRange(String key, double defaultValue, double min, double max) {
-            return BUILDER.comment(fromConfigKey(key), noteFromConfigKey(key)).defineInRange(key, defaultValue, min, max);
-        }
-
-        @SafeVarargs
-        public final <V extends Enum<V>> ForgeConfigSpec.EnumValue<V> defineEnum(String key, V defaultValue, V... enums) {
-            return BUILDER.comment(fromConfigKey(key), noteFromConfigKey(key)).defineEnum(key, defaultValue, enums);
-        }
-
-        /**
-         * Gets translation component from registry as:
-         * config.<key>
-         *
-         * @param key registry key
-         * @return translation component
-         */
-        private static String fromConfigKey(String key) {
-            return new TranslationTextComponent("config." + key).getUnformattedComponentText();
-        }
-
-        /**
-         * Gets translation component note from registry as
-         * config.<key>.note
-         *
-         * @param key registry key
-         * @return translation component
-         */
-        private static String noteFromConfigKey(String key) {
-            return fromConfigKey("prefix.note") + ": " + fromConfigKey(key + ".note");
-        }
     }
 
     public enum CarveType {
