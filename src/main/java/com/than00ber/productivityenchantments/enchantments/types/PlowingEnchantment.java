0e384ff9b99aa2ddc93180ddb32bc21c12a0e96f
greffnoah <greffnoah@gmail.com>
2021-03-16 14:30:27 +0100

    fixed plowing when attempting to plow invalid block

--- src/main/java/com/than00ber/productivityenchantments/enchantments/types/PlowingEnchantment.java
+++ src/main/java/com/than00ber/productivityenchantments/enchantments/types/PlowingEnchantment.java
@@ -4,8 +4,8 @@ import com.than00ber.productivityenchantments.Configs;
 import com.than00ber.productivityenchantments.enchantments.CarvedVolume;
 import com.than00ber.productivityenchantments.enchantments.CarverEnchantmentBase;
 import com.than00ber.productivityenchantments.enchantments.IRightClickEffect;
+import com.than00ber.productivityenchantments.enchantments.IValidatorCallback;
 import net.minecraft.block.*;
-import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -13,7 +13,6 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.ToolType;
 
 import java.util.Collections;
@@ -30,8 +29,8 @@ public class PlowingEnchantment extends CarverEnchantmentBase implements IRightC
 
     @Override
     public boolean isBlockValid(BlockState state, World world, BlockPos pos, ItemStack stack, ToolType type) {
-        Block above = world.getBlockState(pos.up()).getBlock();
-        return state.isIn(Tags.Blocks.DIRT) && (above == Blocks.AIR || above instanceof TallGrassBlock || above instanceof DoublePlantBlock);
+        boolean aboveIsAir = world.getBlockState(pos.up()).getBlock() == Blocks.AIR;
+        return aboveIsAir && (state.getBlock() == Blocks.DIRT || state.getBlock() == Blocks.GRASS_BLOCK);
     }
 
     @Override
@@ -42,9 +41,18 @@ public class PlowingEnchantment extends CarverEnchantmentBase implements IRightC
             if (!player.isSneaking() || !player.isCrouching()) {
                 int radius = enchantment.getMaxEffectiveRadius(level);
 
+                IValidatorCallback callback = new IValidatorCallback() {
+                    @Override
+                    public boolean isBlockValid(BlockState state, World world, BlockPos pos, ItemStack stack, ToolType type) {
+                        Block above = world.getBlockState(pos.up()).getBlock();
+                        boolean isDirty = state.getBlock() == Blocks.DIRT || state.getBlock() == Blocks.GRASS_BLOCK;
+                        return isDirty && (above == Blocks.AIR || above instanceof TallGrassBlock || above instanceof DoublePlantBlock);
+                    }
+                };
+
                 CarvedVolume area = new CarvedVolume(CarvedVolume.Shape.DISC, radius, origin, world)
                         .setToolRestrictions(stack, PLOWING.getToolType())
-                        .filterViaCallback(PLOWING);
+                        .filterViaCallback(callback);
 
                 if (PLOWING_CARVE_TYPE.get().equals(Configs.CarveType.CONNECTED))
                     area.filterConnectedRecursively();
